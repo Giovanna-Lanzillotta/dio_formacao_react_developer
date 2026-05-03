e53128df4d5edc5b30abd9e3730aa76c06589c4b
Giovanna Lanzillotta <lanzillottagigi@gmail.com>
2026-05-03 19:30:34 -0300

    desafio calculadora preparando container

---
+++ 01_Fundamentos_do_react/06_calculadora/desafio01-calculadora/src/App.js
@@ -0,0 +1,14 @@
+import {Container, Content} from './styles'
+
+const App = () => {
+  return (
+    <Container>
+      <Content>
+      Olá
+      </Content>
+      
+    </Container>
+  );
+}
+
+export default App;
