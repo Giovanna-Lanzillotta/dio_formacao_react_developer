22a32a3a5f3464a994f438a8a973299aaae489f5
Giovanna Lanzillotta <lanzillottagigi@gmail.com>
2026-05-05 22:22:00 -0300

    desafio calculaora - desenvolvendo interações nos botões

--- 01_Fundamentos_do_react/06_calculadora/desafio01-calculadora/src/components/Button/style.js
+++ 01_Fundamentos_do_react/06_calculadora/desafio01-calculadora/src/components/Button/style.js
@@ -2,11 +2,15 @@ import styled from 'styled-components';
 
 export const ButtonContainer = styled.button`
     padding: 20px;
-    border: 1px solid #CDCDCD;
+    border: 1px solid #a8a8a8;
     background-color: #DCDCDC;
     color: #000833;
     font-size: 24px;
     font-weight: 700;
     font-family: 'Roboto';
     flex: 1;
+
+    &:hover{
+    opacity: 0.6;
+    }
 `
